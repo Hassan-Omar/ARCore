3012b17e9d02a1d8860937313b2714846c660465
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2020-05-25 23:16:26 +0200

    ADD UI OF Main app

---
+++ Assets/Scriptes/ColorSwitchManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ColorSwitchManager : MonoBehaviour
+{
+    /// <summary>
+    /// Function to Switch the Muterial on Current Selected Model
+    /// </summary>
+    /// <param name="id"> id of target material</param>
+    public void switchToMaterialByID(int id)
+    {
+
+    }
+}
---
+++ Assets/Scriptes/ModelControlManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelControlManager : MonoBehaviour
+{
+    Transform transformOfLastModel; 
+    public void increaseRotation()
+    {
+        updatetransformOfLastModel();
+        transformOfLastModel.Rotate(transform.up * 5);
+    }
+    public void decreaseRotation()
+    {
+        updatetransformOfLastModel();
+        transformOfLastModel.Rotate(-transform.up * 5);
+    }
+    public void increaseScale()
+    {
+        updatetransformOfLastModel();
+        transformOfLastModel.localScale += Vector3.one * 0.15f;
+    }
+    public void decreaseScale()
+    {
+        updatetransformOfLastModel();
+        transformOfLastModel.localScale -= Vector3.one * 0.15f;
+    }
+    private void updatetransformOfLastModel()
+    {
+        // assign New Value 
+        this.transformOfLastModel = GameObject.Find("7777").transform;
+    }
+}
