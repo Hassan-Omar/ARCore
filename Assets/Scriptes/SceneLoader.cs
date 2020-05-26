8bc00fc03a7ebf03ebd61d3eb87e1b5b6cfdc9e2
Hassan Omar <49106965+Hassan-Omar@users.noreply.github.com>
2020-05-26 22:24:07 +0200

    add Start Scene

---
+++ Assets/Scriptes/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName(string name)
+    {
+        SceneManager.LoadSceneAsync(name);
+    }
+}
